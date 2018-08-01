b71e7fbcdd2dcc6b0c13aa96071fb3b1f22f0438
JohnPaul Gitata <johnpaulgitata@gmail.com>
2018-08-01 20:58:15 +0300

    make it better

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     General configuration parent class
     '''
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
-    # https://api.themoviedb.org/3/movie/550?api_key=e75ebc3f9b1295e781106c8a49fe1edb
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
